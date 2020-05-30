cfacbe4898bc2fd2e7ad5b67297e02ae4010d8a8
X360pro <43405739+X360pro@users.noreply.github.com>
2020-05-30 21:05:46 +0530

    Update resultService_test.go

--- service/resultService_test.go
+++ service/resultService_test.go
@@ -1,174 +1,95 @@
 package service
 
-import (
-	"components"
-	"testing"
-)
+import "components"
 
-func TestCheckRow(t *testing.T) {
-	tests := []struct {
-		input    *ResultService
-		mark     string
-		expected bool
-	}{
-		{&ResultService{&BoardService{&components.Board{
-			Size: 2,
-			Cells: []*components.Cell{
-				{Mark: components.XMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-				{Mark: components.OMark},
-			},
-		},
-		},
-		}, components.XMark, true},
-
-		{&ResultService{&BoardService{&components.Board{
-			Size: 3,
-			Cells: []*components.Cell{
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-				{Mark: components.NoMark},
-				{Mark: components.NoMark},
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-			},
-		},
-		},
-		}, components.OMark, true},
-	}
-	for _, test := range tests {
-		if test.input.CheckRow(test.mark) != test.expected {
-			t.Error("check row failed")
-		}
-	}
+type ResultService struct {
+	*BoardService
 }
 
-func TestCheckColumn(t *testing.T) {
-	tests := []struct {
-		input    *ResultService
-		mark     string
-		expected bool
-	}{
-		{&ResultService{&BoardService{&components.Board{
-			Size: 2,
-			Cells: []*components.Cell{
-				{Mark: components.XMark},
-				{Mark: components.OMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-			},
-		},
-		},
-		}, components.XMark, true},
-
-		{&ResultService{&BoardService{&components.Board{
-			Size: 3,
-			Cells: []*components.Cell{
-				{Mark: components.OMark},
-				{Mark: components.XMark},
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-				{Mark: components.OMark},
-				{Mark: components.NoMark},
-				{Mark: components.NoMark},
-			},
-		},
-		},
-		}, components.OMark, true},
-	}
-	for _, test := range tests {
-		if test.input.CheckColumn(test.mark) != test.expected {
-			t.Error("check column failed")
-		}
-	}
+func NewResultService(boardService *BoardService) *ResultService {
+	return &ResultService{boardService}
 }
 
-func TestCheckLRDiagonal(t *testing.T) {
-	tests := []struct {
-		input    *ResultService
-		mark     string
-		expected bool
-	}{
-		{&ResultService{&BoardService{&components.Board{
-			Size: 2,
-			Cells: []*components.Cell{
-				{Mark: components.XMark},
-				{Mark: components.OMark},
-				{Mark: components.NoMark},
-				{Mark: components.XMark},
-			},
-		},
-		},
-		}, components.XMark, true},
-
-		{&ResultService{&BoardService{&components.Board{
-			Size: 3,
-			Cells: []*components.Cell{
-				{Mark: components.XMark},
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-				{Mark: components.NoMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-				{Mark: components.NoMark},
-				{Mark: components.XMark},
-				{Mark: components.XMark},
-			},
-		},
-		},
-		}, components.XMark, true},
-	}
-	for _, test := range tests {
-		if test.input.CheckLRDiagonal(test.mark) != test.expected {
-			t.Error("check LR diagonal failed")
+func (r *ResultService) checkRow(mark string) bool {
+	//for rows
+	count := 0
+	for i := 0; i < int(r.Size*r.Size); i++ {
+		if count == int(r.Size) {
+			return true
+		}
+		if r.Cells[i].GetMark() == mark {
+			count++
+		}
+		if i+1%int(r.Size) == 0 {
+			count = 0
 		}
 	}
+	return false
+}
+func (r *ResultService) checkColumn(mark string) bool {
+	//for columns
+	count_col := make([]int, r.Size)
+	for i := 0; i < int(r.Size*r.Size); i++ {
+
+		if r.Cells[i].GetMark() == mark {
+			count_col[i%int(r.Size)]++
+		}
+		if contains(count_col, int(r.Size)) {
+			return true
+		}
+	}
+	return false
+}
+func (r *ResultService) checkLRDiagonal(mark string) bool {
+	//for LR diagonal
+	count := 0
+	j := 0
+	for i := 0; i < int(r.Size); i++ {
+
+		if r.Cells[(i*int(r.Size))+j].GetMark() == mark {
+			count++
+		}
+		if count == int(r.Size) {
+			return true
+		}
+		j++
+	}
+	return false
+}
+func (r *ResultService) checkRLDiagonal(mark string) bool {
+	//for RL diagonal
+	count := 0
+	for i := 0; i < int(r.Size); i++ {
+		if r.Cells[(i+1)*(int(r.Size)-1)].GetMark() == mark {
+			count++
+		}
+		if count == int(r.Size) {
+			return true
+		}
+	}
+	return false
+}
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
-func TestCheckRLDiagonal(t *testing.T) {
-	tests := []struct {
-		input    *ResultService
-		mark     string
-		expected bool
-	}{
-		{&ResultService{&BoardService{&components.Board{
-			Size: 2,
-			Cells: []*components.Cell{
-				{Mark: components.NoMark},
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-				{Mark: components.XMark},
-			},
-		},
-		},
-		}, components.OMark, true},
+func (r *ResultService) GiveResult(player *components.Player) bool {
 
-		{&ResultService{&BoardService{&components.Board{
-			Size: 3,
-			Cells: []*components.Cell{
-				{Mark: components.NoMark},
-				{Mark: components.OMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-				{Mark: components.XMark},
-				{Mark: components.NoMark},
-				{Mark: components.XMark},
-				{Mark: components.OMark},
-				{Mark: components.OMark},
-			},
-		},
-		},
-		}, components.XMark, true},
+	if r.checkRow(player.Mark) {
+		return true
+	} else if r.checkColumn(player.Mark) {
+		return true
+	} else if r.checkLRDiagonal(player.Mark) {
+		return true
+	} else if r.checkLRDiagonal(player.Mark) {
+		return true
+	} else if r.CheckBoardIsFull() {
+		return true
 	}
-	for _, test := range tests {
-		if test.input.CheckRLDiagonal(test.mark) != test.expected {
-			t.Error("check RL diagonal failed")
-		}
-	}
+	return false
 }
